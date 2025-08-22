b66dec8c4ce17cccdf042933fc5ad1af767f9a3b
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-08-22 09:31:41 +0100

    connection with database

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'Europe/Lisbon',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  timezone: 'Europe/Lisbon',
+};
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Lukas',
+      sobrenome: 'Sperandio',
+      email: 'lukassperandio@gmail.com',
+      idade: 18,
+      peso: 70.5,
+      altura: 1.72,
     });
+    res.json(novoAluno);
   }
 }
 
