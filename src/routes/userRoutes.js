a0d184d9f87bb6b68b88c839045671e21bfe2385
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-08-23 11:53:56 +0100

    Security fixes for users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,10 @@ import loginRequired from '../middleware/loginRequired';
 const router = new Router();
 
 router.post('/', userController.store);
-router.get('/', loginRequired, userController.index);
+router.get('/', userController.index);
 router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
 
