1b7726e22a80c0fcf6c43bfad702514579b01195
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-08-24 09:29:41 +0100

    Student route created

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Lukas',
-      sobrenome: 'Sperandio',
-      email: 'lukassperandio@gmail.com',
-      idade: 18,
-      peso: 70.5,
-      altura: 1.72,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,8 +4,9 @@ import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
-router.post('/', userController.store);
-router.get('/', userController.index);
+/* router.post('/', userController.store);
+router.get('/', userController.index); */
+
 router.get('/:id', userController.show);
 router.put('/:id', loginRequired, userController.update);
 router.delete('/:id', loginRequired, userController.delete);
