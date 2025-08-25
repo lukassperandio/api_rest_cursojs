c37af2ca0d5e825900fbc67c51ccaf530a60cf72
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-08-25 07:02:13 +0100

    Photo upload route

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,16 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ errors: [err.code] });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
