064fbccd2303512bbbf54e7604c42f7ac086a42e
Lukas Sperandio <emanuelsperandio@gmail.com>
2025-08-26 10:02:53 +0100

    Adding files

---
+++ dist/database/seeders/20250824080414-criar-usuarios.js
@@ -0,0 +1,33 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Lukas',
+        email: 'lukas@example.com',
+        password_hash: await bcryptjs.hashSync('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Lukas2',
+        email: 'lukas2@example.com',
+        password_hash: await bcryptjs.hashSync('654321', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Lukas3',
+        email: 'lukas3@example.com',
+        password_hash: await bcryptjs.hashSync('741025', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
